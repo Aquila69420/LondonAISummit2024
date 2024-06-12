876b76955f6319e98c999f845c2f3211e3053e1c
mnokno <prv.nowacki@gmail.com>
2024-06-12 16:16:47 +0100

    prompt

--- p/document_analysis.py
+++ p/document_analysis.py
@@ -1,12 +1,12 @@
 from gemini_model import GeminiModel
 from custome_io import read_docx
 
-
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
     a = read_docx("input/IBP_Problemstatement.docx")
     gemini_model = GeminiModel("AIzaSyCO8QBl6pLBM3XIxh33voc0JlC5w0J6AAU", "gemini-1.5-pro")
-    prompt = "[TASK] relevant data points."
+    with open("input/prompt.txt", "r") as f:
+        prompt = f.read()
     gemini_model.start_chat()
     response = gemini_model.send_message(a + prompt)
     print(response)
