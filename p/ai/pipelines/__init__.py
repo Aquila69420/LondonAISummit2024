20e6230478780a73461d641b9e1ab2a689d8cfb8
mnokno <prv.nowacki@gmail.com>
2024-06-13 02:21:06 +0100

    Added missing __init__ implementations

---
+++ p/ai/pipelines/__init__.py
@@ -0,0 +1,5 @@
+from .understand_prt_pipeline import UnderstandPrtPipeline
+from .user_data_extraction_pipeline import UserDataExtractionPipeline
+from .recommend_revaluation_pipeline import RecommendRevaluationPipeline
+
+__all__ = ['UnderstandPrtPipeline', 'UserDataExtractionPipeline', 'RecommendRevaluationPipeline']
